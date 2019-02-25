bb75daeb9990bfce5e687e2eed964785ede78c40
rosssimargh <rossimar.g.h.t.k.m@gmail.com>
2019-02-25 09:57:36 -0400

    trabajos

---
+++ OrdenamientoInsercion/OrdenamientoInsercion/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System;
+
+
+namespace OrdenamientoInsercion
+{
+    class Program
+    {
+        class PV
+        {
+            private int[] VECTOR;
+            public void CARGAR()
+            {
+                Console.WriteLine("METODO DE INTERSECCION");
+                Console.WriteLine("Cuantos longitud de vector:");
+                string LINEA;
+                LINEA = Console.ReadLine();
+                int CANT;
+                CANT = int.Parse(LINEA);
+                VECTOR = new int[CANT];
+                for (int F = 0; F < VECTOR.Length; F++)
+                {
+                    Console.WriteLine("INGRESE ELEMENTO" + (F + 1) + ":");
+                    LINEA = Console.ReadLine();
+                    VECTOR[F] = int.Parse(LINEA);
+
+                }
+            }
+            public void INTERCESIONDIRECTA()
+            {
+                int AUX;
+                int J;
+                for(int I = 0; I< VECTOR.Length; I++)
+                {
+                    AUX = VECTOR[I];
+                    J = I - 1;
+                    while(J>= 0 && VECTOR[J] > AUX)
+                    {
+                        VECTOR[J + 1] = VECTOR[J];
+                        J--;
+                    }
+                    VECTOR[J + 1] = AUX;
+                }
+            }
+
+            public void IMPRIMIR()
+            {
+                Console.WriteLine("Vector ordenados en forma ascendente");
+                for(int f= 0; f< VECTOR.Length;f++)
+                {
+                    Console.Write(VECTOR[f] + "-");
+                }
+                Console.ReadKey();
+            }
+        }
+        static void Main(string[] args)
+        {
+            PV prueba = new PV();
+            prueba.CARGAR();
+            prueba.INTERCESIONDIRECTA();
+            prueba.IMPRIMIR();
+        }
+    }
+}
---
+++ linq2/linq2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            personas[] alumnos = new personas[7];
+            alumnos[0] = new personas("ROSSIMAR", "GUZMAN", 18, "FEMENINO", "1234556", 1.54, 40);
+            alumnos[1] = new personas("Carolina", "Lopez", 19, "FEMENINO", "1234556", 1.65, 60);
+            alumnos[2] = new personas("Diegp", "Latorre", 18, "Nodefinido", "1234556", 1.65, 60);
+            alumnos[3] = new personas("Dieter", "Quiñones", 19, "FEME", "1234556", 1.59, 60);
+            alumnos[4] = new personas("Guztavo", "Zarate", 19, "FEME", "1234556", 1.79, 70);
+            alumnos[5] = new personas("Alejandro", "Castro", 19, "FEME", "1234556", 1.64, 50);
+            alumnos[6] = new personas("Franko", "Adamczyk", 18, "FEME", "1234556", 1.70, 60);
+            //lista de todos los alumnos
+            var consulta1 = from alm in alumnos
+                            select alm;
+            //lista de los alumnos cuya edad sea igual a 18
+
+            var c2 = from alm in alumnos
+                     where alm.Edad > 18
+                     select alm;
+            /////////////////////////////////////////////
+
+            var c3 = from alm in alumnos
+                     orderby alm.Apellidos
+                     select alm;
+            ////////////////////////////////////////////
+            var c4 = from alm in alumnos
+                     orderby alm.Apellidos
+                     /////////////////////////////
+                     select new { alm.Nombre, alm.Apellidos };
+            var c6 = from alm in alumnos
+                     orderby alm.Nombre, alm.Apellidos descending
+                     select alm;
+            //////////////////////////////////////////////////////////
+            var c5 = from alm in alumnos
+                     group alm by alm.Edad into edad
+                     select edad;
+            //grupo edades 
+            foreach(var grupoedades in c5)
+            {
+                Console.WriteLine(grupoedades.Key);
+                foreach(personas alumno in grupoedades)
+                    {
+                    Console.WriteLine("{0} {1}", alumno.Nombre, alumno.Apellidos);
+                }
+            }
+            Console.WriteLine("************************************************");
+
+            //7.-Edades de los alumnos y cantidad
+            var c7 = from alm in alumnos
+                     group alm by alm.Edad into edad
+                     select new { Edad = edad.Key, Cantidad = edad.Count() };
+
+            foreach (var grupoedad in c7)
+            {
+                Console.WriteLine("{0} {1}", grupoedad.Edad, grupoedad.Cantidad);
+            }
+
+          //  foreach(var alumno in c2)
+           // {
+            //    Console.WriteLine("{0} {1}",alumno.Nombre,alumno.Apellidos);
+         //   }
+            Console.ReadKey();
+        }
+    }
+}
