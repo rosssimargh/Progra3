a7c765bbe0f3d33d376c05ccfd985ab3b81c37d4
rosssimargh <rossimar.g.h.t.k.m@gmail.com>
2019-02-27 08:38:15 -0400

    proyectos

---
+++ Pilas/Pilas/Class pila.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pilas
+{
+    class Class_pila<T>
+    {
+        
+        private T[]arreglo = new T[10];
+        private int tope = -1;
+        public void Push(T valor)
+        {
+            if (!llenar())
+            {
+                tope++;
+              arreglo[tope] = valor;
+            }
+            else
+            {
+                Console.WriteLine("La pila esta llena");
+            }
+
+        }
+
+        public T pop()
+        {
+            if(!Vacia())
+            {
+                tope--;
+                return arreglo[tope + 1];
+            }
+            else
+            {
+                Console.WriteLine("La pila esta vacia");
+                return arreglo[tope + 1];
+            }
+
+        }
+
+        private Boolean Vacia()
+        {
+            return tope == -1;
+        }
+        private Boolean llenar()
+        {
+            return tope == 10;
+        }
+
+        public void Mostrar()
+        {
+            for (int i=0; i< tope+1;i++)
+            {
+                Console.WriteLine(arreglo[i] + ".");
+            }
+
+        }
+    }
+}
---
+++ Pilas/Pilas/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Pilas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0 , a =0;
+           Class_pila<int> p = new Class_pila<int>();
+            while(opcion!=4)
+            {
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Mostrar");
+                Console.WriteLine("4. Salir");
+                opcion = int.Parse(Console.ReadLine());
+                switch(opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Introduzca elemento: ");
+                        a = int.Parse(Console.ReadLine());
+                        p.Push(a);
+                        Console.WriteLine("***********************************");
+                        break;
+
+                    case 2:
+                        a = p.pop();
+                        Console.WriteLine("Elemento eliminado: "+ a);
+
+                        break;
+                    case 3:
+                        Console.WriteLine("***********************************");
+                        p.Mostrar();
+                        Console.WriteLine();
+                        break;
+                    case 4:
+
+                        break;
+
+                }
+                Console.ReadKey();
+
+            }
+
+        }
+    }
+}
