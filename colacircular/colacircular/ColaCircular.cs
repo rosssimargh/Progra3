ad199de1394addcecaa9c4778e286a62a7cab0c7
rosssimargh <rossimar.g.h.t.k.m@gmail.com>
2019-03-06 08:37:30 -0400

    Poyectos

---
+++ colacircular/colacircular/ColaCircular.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace colacircular
+{
+    class ColaCircular<T>
+    {
+        public const int Maximo = 10;
+        private T[] arreglo = new T[10];
+        private int fin = 0;
+        private int frente = 0;
+
+        public void insertar(T valor)
+        {
+            if (!llenar())
+            {
+                arreglo[fin] = valor;
+                fin = (fin + 1) % Maximo;
+            }
+            else
+            {
+                Console.WriteLine("La Cola esta llena");
+            }
+
+        }
+        public T eliminar()
+        {
+            if (!Vacia())
+            {
+                T valor;
+                valor = arreglo[frente];
+                frente = (frente + 1) % Maximo;
+                return valor;
+            }
+            else
+            {
+                Console.WriteLine("La Cola esta vacia");
+                return arreglo[frente ];
+            }
+
+        }
+        public void Mostrar()
+        {
+            int aux = frente;
+            while(aux != fin)
+            {
+                Console.WriteLine(arreglo[aux]+ " ");
+                aux = (aux + 1) % Maximo;
+            }
+
+
+        }
+        private Boolean Vacia()
+        {
+            return frente == fin;
+        }
+        private Boolean llenar()
+        {
+            return fin == (fin+1)%Maximo;
+        }
+    }
+}
---
+++ colacircular/colacircular/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace colacircular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0, opcion = 0;
+
+
+
+
+            //HACEMOS LA COLA
+            ColaCircular<int> p = new ColaCircular<int>();
+            while (opcion != 4)
+            {
+                Console.WriteLine("1. insertar");
+                Console.WriteLine("2. eliminar");
+                Console.WriteLine("3. Mostrar");
+                Console.WriteLine("4. Salir");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Introduzca elemento: ");
+                        n = int.Parse(Console.ReadLine());
+                        p.insertar(n);
+
+
+
+                        Console.WriteLine("***********************************");
+                        break;
+
+                    case 2:
+                        n = p.eliminar();
+                        Console.WriteLine("Elemento eliminado: " + n);
+
+
+                        break;
+                    case 3:
+                        Console.WriteLine("***********************************");
+                        p.Mostrar();
+                        Console.WriteLine();
+
+                        break;
+                    case 4:
+
+                        break;
+
+                }
+                Console.ReadKey();
+
+            }
+
+        }
+    }
+}
