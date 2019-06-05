936e789bfe3b93891fc2c53ef0b77b9076af41e1
rosssimargh <rossimar.g.h.t.k.m@gmail.com>
2019-06-05 08:49:10 -0400

    tarea2

---
+++ apptresenraya/apptresenraya/MainActivity.cs
@@ -0,0 +1,163 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace apptresenraya
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.layout.activity_main);
+           
+            TextView resultado = FindViewById<TextView>(Resources.Id.txtResultado1);
+            ImageButton i00 = FindViewById<ImageButton>(Resource.Id.img00);
+            ImageButton i01 = FindViewById<ImageButton>(Resource.Id.img01);
+            ImageButton i02 = FindViewById<ImageButton>(Resource.Id.img02);
+            ImageButton i10 = FindViewById<ImageButton>(Resource.Id.img10);
+            ImageButton i11 = FindViewById<ImageButton>(Resource.Id.img11);
+            ImageButton i12 = FindViewById<ImageButton>(Resource.Id.img12);
+            ImageButton i20 = FindViewById<ImageButton>(Resource.Id.img20);
+            ImageButton i21 = FindViewById<ImageButton>(Resource.Id.img21);
+            ImageButton i22 = FindViewById<ImageButton>(Resource.Id.img22);       
+            TextView turno = FindViewById<TextView>(Resource.Id.txtTurno);
+            Button reinicio = FindViewById<Button>(Resource.Id.button1);
+            turno.Text = "x";
+            void AnalizaMensage(string mensaje)
+            {
+
+                string[] respuesta = mensaje.Split(':');
+                switch (respuesta[0])
+                {
+                    case "ganador":
+                        resultado.Text = "el ganador es: " + respuesta[1];
+                        break;
+                    case "continuar":
+                        if (turno.Text == "x")
+                            turno.Text = "o";
+                        else
+                            turno.Text = "x";
+                        break;
+                    case "error":
+                        resultado.Text = "error: " + respuesta[1];
+                        break;
+                    case "resultado":
+                        resultado.Text = "ha habido un empate";
+                        break;
+
+                }
+            }
+           
+            i00.Click += (sender, e) =>
+            {
+                string respuesta = Raya.getInstancia().marcar(0, 0, turno.Text[0]);
+                if (turno.Text == "x")
+                    i00.SetImageResource(Resource.Drawable.X);
+
+                else
+                    i00.SetImageResource(Resource.Drawable.O);
+                AnalizaMensage(respuesta);
+            };
+            i01.Click += (sender, e) =>
+            {
+                string respuesta = Raya.getInstancia().marcar(0, 1, turno.Text[0]);
+                if (turno.Text == "x")
+                    i01.SetImageResource(Resource.Drawable.X);
+
+                else
+                    i01.SetImageResource(Resource.Drawable.O);
+                AnalizaMensage(respuesta);
+            };
+            i02.Click += (sender, e) =>
+            {
+                string respuesta = Raya.getInstancia().marcar(0, 2, turno.Text[0]);
+                if (turno.Text == "x")
+                    i02.SetImageResource(Resource.Drawable.X);
+
+                else
+                    i02.SetImageResource(Resource.Drawable.O);
+                AnalizaMensage(respuesta);
+            };
+            i10.Click += (sender, e) =>
+            {
+                string respuesta = Raya.getInstancia().marcar(1, 0, turno.Text[0]);
+                if (turno.Text == "x")
+                    i10.SetImageResource(Resource.Drawable.X);
+
+                else
+                    i10.SetImageResource(Resource.Drawable.O);
+                AnalizaMensage(respuesta);
+            };
+            i11.Click += (sender, e) =>
+            {
+                string respuesta = Raya.getInstancia().marcar(1, 1, turno.Text[0]);
+                if (turno.Text == "x")
+                    i11.SetImageResource(Resource.Drawable.X);
+
+                else
+                    i11.SetImageResource(Resource.Drawable.O);
+                AnalizaMensage(respuesta);
+            };
+            i12.Click += (sender, e) =>
+            {
+                string respuesta = Raya.getInstancia().marcar(1, 2, turno.Text[0]);
+                if (turno.Text == "x")
+                    i12.SetImageResource(Resource.Drawable.X);
+
+                else
+                    i12.SetImageResource(Resource.Drawable.O);
+                AnalizaMensage(respuesta);
+            };
+            i20.Click += (sender, e) =>
+            {
+                string respuesta = Raya.getInstancia().marcar(2, 0, turno.Text[0]);
+                if (turno.Text == "x")
+                    i20.SetImageResource(Resource.Drawable.X);
+
+                else
+                    i20.SetImageResource(Resource.Drawable.O);
+                AnalizaMensage(respuesta);
+            };
+            i21.Click += (sender, e) =>
+            {
+                string respuesta = Raya.getInstancia().marcar(2, 1, turno.Text[0]);
+                if (turno.Text == "x")
+                    i21.SetImageResource(Resource.Drawable.X);
+
+                else
+                    i21.SetImageResource(Resource.Drawable.O);
+                AnalizaMensage(respuesta);
+            };
+            i22.Click += (sender, e) =>
+            {
+                string respuesta = Raya.getInstancia().marcar(2, 2, turno.Text[0]);
+                if (turno.Text == "x")
+                    i22.SetImageResource(Resource.Drawable.X);
+
+                else
+                    i22.SetImageResource(Resource.Drawable.O);
+                AnalizaMensage(respuesta);
+            };
+            reinicio.Click += (sender, e) =>
+              {
+                  Raya.getInstancia().Reiniciar();
+                  i00.SetImageResource(Resource.Drawable.vacia);
+                  i01.SetImageResource(Resource.Drawable.vacia);
+                  i02.SetImageResource(Resource.Drawable.vacia);
+                  i10.SetImageResource(Resource.Drawable.vacia);
+                  i11.SetImageResource(Resource.Drawable.vacia);
+                  i12.SetImageResource(Resource.Drawable.vacia);
+                  i20.SetImageResource(Resource.Drawable.vacia);
+                  i21.SetImageResource(Resource.Drawable.vacia);
+                  i22.SetImageResource(Resource.Drawable.vacia);
+                  turno.Text = "x";
+                  resultado.Text = "";
+              };
+        }
+    }
+}
---
+++ apptresenraya/apptresenraya/TresEnRaya.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace apptresenraya
+{
+    class Raya
+    {
+        private static Raya Instancia;
+        private Char[,] tabla;
+        private int turno;
+        private Raya()
+        {
+            turno = 1;
+            tabla = new char[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                { tabla[i, j] = '-'; }
+            }
+        }
+
+        public static Raya getInstancia()
+        {
+            if (Instancia == null)
+                Instancia = new Raya();
+
+            return Instancia;
+        }
+        public void Reiniciar()
+        {
+            turno = 1;
+            tabla = new char[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                { tabla[i, j] = '-'; }
+            }
+        }
+
+        public String marcar(int x, int y, Char marca)
+        {
+            int turnomarcacion = 0;
+            if (marca == 'x')
+                turnomarcacion = 1;
+            else
+                turnomarcacion = 2;
+            // verificar si el turno corresponde
+            if (turnomarcacion == turno)
+            {
+                // si esta vacio marcar
+                if (tabla[x, y] == '-')
+                {
+                    tabla[x, y] = marca;
+                    // verificar si hy ganador
+                    if (ganador(marca))
+                    {
+                        return "ganador:" + marca.ToString();
+
+                    }
+                    else
+                    {
+                        // verificar si hay empate
+                        if (empate())
+                        {
+                            return "resultado:empate";
+                        }
+                        else
+                        {
+                            // pasar el turno
+                            turno = (turno % 2) + 1;
+                            return "continuar:juego";
+                        }
+                    }
+
+                }
+                else
+                    return "error:posicion ocupada";
+            }
+            else
+                return "error:turno incorrecto";
+        }
+
+        private Boolean ganador(char marca)
+        {
+            Boolean bganador = false;
+            // verificar si hay ganador por filas
+            if (tabla[0, 0] == marca && tabla[0, 1] == marca && tabla[0, 2] == marca)
+                bganador = true;
+            if (tabla[1, 0] == marca && tabla[1, 1] == marca && tabla[1, 2] == marca)
+                bganador = true;
+            if (tabla[2, 0] == marca && tabla[2, 1] == marca && tabla[2, 2] == marca)
+                bganador = true;
+            // verificar sihay ganadorpor columnas
+            if (tabla[0, 0] == marca && tabla[1, 0] == marca && tabla[2, 0] == marca)
+                bganador = true;
+            if (tabla[0, 1] == marca && tabla[1, 1] == marca && tabla[2, 1] == marca)
+                bganador = true;
+            if (tabla[0, 2] == marca && tabla[1, 2] == marca && tabla[2, 2] == marca)
+                bganador = true;
+            // verificar diagonales
+            if (tabla[0, 0] == marca && tabla[1, 1] == marca && tabla[2, 2] == marca)
+                bganador = true;
+            if (tabla[2, 0] == marca && tabla[1, 1] == marca && tabla[0, 2] == marca)
+                bganador = true;
+            return bganador;
+
+        }
+        private Boolean empate()
+        {
+            Boolean bempate = true;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    if (tabla[i, j] == '-')
+                        bempate = false;
+            return bempate;
+        }
+    }
+}
